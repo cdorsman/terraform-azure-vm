bf8e7d3f1311d71d153b0f3c63570be416b80c1b
Chris Dorsman <39407105+cdorsman@users.noreply.github.com>
2025-09-12 00:06:02 +0200

    new

--- providers.tf
+++ providers.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = ">= 1.8.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>4.1.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
