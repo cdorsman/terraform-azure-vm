9103eac0c4fc961bb753982913dabaa3b68dc321
Chris Dorsman <39407105+cdorsman@users.noreply.github.com>
2025-09-12 02:19:49 +0200

    added ssh configV4

--- modules/vmss/main.tf
+++ modules/vmss/main.tf
@@ -13,8 +13,8 @@ resource "azurerm_linux_virtual_machine_scale_set" "this" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
     version   = "latest"
   }
 
